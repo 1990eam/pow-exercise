df0e276f91e0bd54ad9a4c19577fb97e5960dcd0
Elian Magni <magni.elian@gmail.com>
2021-04-02 15:42:26 -0300

    Authentication required for home path. Prepare home view to create Posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!
 
   def home
+    @post = Post.new
   end
 end
