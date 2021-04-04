60b07deba0da1e154ce48d68e1d0932f327f2e58
Elian Magni <magni.elian@gmail.com>
2021-04-04 19:34:10 -0300

    Added after_create logic to send Telegram message upon post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :content, length: { in: 1..400 }
+
+  after_create do
+    adapter = TelegramAdapter.new
+    adapter.send_message(self.content)
+  end
 end
